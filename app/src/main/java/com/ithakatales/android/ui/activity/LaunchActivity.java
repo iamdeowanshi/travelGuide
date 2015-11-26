81ca77cb42c19591f9d4e1a67be3d620e80e9529
Farhan Ali <farhan.ali@tecsolsoftware.com>
2015-11-26 17:35:45 +0530

    Created launch activity with appropriate resources

---
+++ app/src/main/java/com/ithakatales/android/ui/activity/LaunchActivity.java
@@ -0,0 +1,31 @@
+package com.ithakatales.android.ui.activity;
+
+import android.os.Bundle;
+
+import com.ithakatales.android.R;
+import com.ithakatales.android.app.base.BaseActivity;
+import com.ithakatales.android.ui.activity.test.TestActivity;
+
+import butterknife.OnClick;
+
+public class LaunchActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launch);
+    }
+
+    @OnClick(R.id.button_login)
+    void launchLogin() {
+        startActivity(TestActivity.class, null);
+        finish();
+    }
+
+    @OnClick(R.id.button_sneak_peak)
+    void launchHomeAsGuest() {
+        startActivity(TestActivity.class, null);
+        finish();
+    }
+
+}
