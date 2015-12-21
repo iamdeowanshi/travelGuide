75f7f32af60e6214a0f6f5dddea605655a405a51
Farhan Ali <farhan.ali@tecsolsoftware.com>
2015-12-21 12:04:31 +0530

    Test activity changes to include ui

--- app/src/main/java/com/ithakatales/android/ui/activity/test/UiTestActivity.java
+++ app/src/main/java/com/ithakatales/android/ui/activity/test/UiTestActivity.java
@@ -5,12 +5,12 @@ import android.os.Bundle;
 import com.ithakatales.android.R;
 import com.ithakatales.android.app.base.BaseActivity;
 
-public class MyToursTestActivity extends BaseActivity {
+public class UiTestActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.test_activity_my_tours);
+        setContentView(R.layout.test_activity_ui);
     }
 
 }
